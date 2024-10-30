3bf03c92935e595172b8ab3b3716525700984166
Sowresh Mecheri-Senthil <138344579+SowreshMS@users.noreply.github.com>
2024-10-30 13:29:47 -0500

    Update resent_config.py

--- config/resent_config.py
+++ config/resent_config.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 
 @dataclass
 class resnet_config:
-    num_layers = 2
+    num_layers = 3
     dropout = 0.1
     num_channels = 3
     output_embed = 64 * (2 ** num_layers)
