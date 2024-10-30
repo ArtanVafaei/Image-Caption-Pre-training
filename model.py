9096d93b8ee934c53b616ad5a2f5acce0b90cc5c
Sowresh Mecheri-Senthil <138344579+SowreshMS@users.noreply.github.com>
2024-10-30 13:53:15 -0500

    Update model.py

--- model.py
+++ model.py
@@ -7,16 +7,18 @@ class VLM(nn.Module):
     def __init__(self, vision_config, language_config):
         super().__init__()
         self.vision_model = Resnet(vision_config)
+        # the layer that takes the image embedding with dimension (N, w' * h', image_dim) and converts it to (N, seq_len, text_dim)
         self.visual_proj = nn.Linear(vision_config.output_embed, language_config.embed_dim)
         self.language_model = GPT(language_config)
         self.language_config = language_config
 
     def forward(self, image, starting_text, labels=None):
-        image = self.vision_model(image).transpose(1, 2)
-        image_embeddings = self.visual_proj(image)[:, :self.language_config.seq_len]
-        x, loss = self.language_model(image_embeddings, starting_text, labels)
+        image = self.vision_model(image).transpose(1, 2)            
+        image_embeddings = self.visual_proj(image)[:, :self.language_config.seq_len]       # We only want up to the sequence length after the projection
+        x, loss = self.language_model(image_embeddings, starting_text, labels)            
         return x, loss
 
+    # Don't worry about this. This basically just uses the model to generate tokens we would later decode to view the output in english. This is what we would use for inferencing.
     @torch.no_grad()
     def generate(self, image, starting_text, max_new_tokens, temperature=1, do_sample=True, top_k=None):
         image = self.vision_model(image).transpose(1, 2)
