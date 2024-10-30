12d7a7fa5e40b4e8ebb85be6c3f7b7d29d0c61fc
Sowresh Mecheri-Senthil <138344579+SowreshMS@users.noreply.github.com>
2024-10-30 13:35:11 -0500

    Update trainer_config.py

--- config/trainer_config.py
+++ config/trainer_config.py
@@ -10,7 +10,7 @@ class trainer_args:
     num_iterations = 100        # epochs --> how many times the model learns from each sample in the data
     gradient_accumulation_steps = 4         # After how many batches will the model's weights be updated
     grad_clip = 1.0
-    lr = 4e-4
+    lr = 4e-4                            # The learning rate --> Might add learning rate scheduler
 
     image_size = (128, 128)
     seq_len = gpt2_config.seq_len
